8db5df21a9d7ab2303f2982898b86ad64b19037e
admin <admin@MORENO-P.onlineformapro.org>
2020-11-15 13:03:17 +0100

    go commit

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
 ]);
 
 /**
- * 2. Récupération des articles
+ * 2. Récupération des articles-
  */
 // On utilisera ici la méthode query (pas besoin de préparation car aucune variable n'entre en jeu)
 $resultats = $pdo->query('SELECT * FROM articles ORDER BY created_at DESC');
